de04bf34524005514066b04cafe73d245aa1933a
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-21 19:24:05 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const mysql=require("mysql");
 const con=mysql.createConnection({
- host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nodeMSDB'
+ host: 'db_host',
+  user: 'db_user',
+  password: 'db_pass',
+  database: 'db_Name' 
 });
 con.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
   });
-  con.query('INSERT INTO users(user, age, name) VALUES ("saziran",42, "Yazd")', (err,insert) => {
-    if(err) throw err;
-    console.log('Data received from Db:');
-    console.log(insert);
-  });
-  con.query('SELECT * FROM users', (err,rows) => {
-    if(err) throw err;
-    console.log('Data received from Db:');
-    console.log(rows[1]);
-  });
+//   con.query('INSERT INTO users(user, age, name) VALUES ("mahdiyarsoft",17, "iran")', (err,insert) => {
+//     if(err) throw err;
+//     console.log('Data received from Db:');
+//     console.log(insert);
+//   });
+//   con.query('SELECT * FROM users', (err,rows) => {
+//     if(err) throw err;
+//     console.log('Data received from Db:');
+//     console.log(rows);
+//   });
